211834223244e601a6a2571cd2ff485e52f6dc74
Narkidae <connorbtaylor@gmail.com>
2013-11-12 20:07:14 -0600

    Finished

--- ScholarOrganization.java
+++ ScholarOrganization.java
@@ -1,8 +1,21 @@
 
 public abstract class ScholarOrganization {
+	/* A variable to store the name of the organization */
 	private String name;
 	
+	/**
+	 * Default constructor
+	 */
 	public ScholarOrganization() { }
+	
+	/**
+	 * Preferred constructor
+	 * @param name the name of the organization
+	 */
+	public ScholarOrganization(String name) {
+		this.name = name;
+	}
+	
 	
 	/**
 	 * Getter for name
