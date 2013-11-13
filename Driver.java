5e5bef4e69a8dbd6aa1e5e8d62f02818489390e4
Narkidae <connorbtaylor@gmail.com>
2013-11-13 14:10:37 -0600

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,9 +1,22 @@
 
 public class Driver {
+	private ScholarshipModel myModel;
+	private ScholarshipModelSelectionView selectionView = new ScholarshipModelSelectionView();
+	//private ScholarshipModelDisplayView selectionView = new ScholarshipModelSelectionView();
+	
+	private ScholarshipModelController myController = new ScholarshipModelController();
 	
 	public static void main(String[] args)
 	{
-		ScholarshipModelSelectionView selectionView = new ScholarshipModelSelectionView();
+		new Driver();
+		
+		//ScholarshipModelSelectionView selectionView = new ScholarshipModelSelectionView();
 		//Initialize components, get everything going
+	}
+	public Driver(){
+		myModel = new ScholarshipModel();
+		selectionView.setModel(myModel);
+		myController.setModel(myModel);
+		myController.setSelectionView(selectionView);
 	}
 }
