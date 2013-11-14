15d9322a50bde1bc999da4b103aaeab82717d0c3
Narkidae <connorbtaylor@gmail.com>
2013-11-13 19:10:41 -0600

    Update ScholarshipModel.java

--- ScholarshipModel.java
+++ ScholarshipModel.java
@@ -7,7 +7,9 @@ import java.util.Arrays;
 public class ScholarshipModel {
 
 	private ArrayList<Scholar> scholars = new ArrayList<Scholar>();
-	private ArrayList<ScholarOrganization> scholarOrganizations;
+	private ArrayList<ScholarOrganization> scholarOrganizations = new ArrayList<ScholarOrganization>();
+	private ArrayList<Paper> papers = new ArrayList<Paper>();
+	
 	private ArrayList<ActionListener> actionListenerList = new ArrayList<ActionListener>();
 	
 	/**
@@ -32,15 +34,6 @@ public class ScholarshipModel {
 	}
 	
 	/**
-	 * Imports data from a specified file
-	 * @param filepath Filepath to import from
-	 */
-	public void importData(String filepath)
-	{
-		
-	}
-	
-	/**
 	 * Loads data from a specified file
 	 * @param filepath Filepath to load from
 	 */
@@ -59,15 +52,6 @@ public class ScholarshipModel {
 	}
 	
 	/**
-	 * Exports data to a specified file
-	 * @param filepath Filepath to export to
-	 */
-	public void exportData(String filepath)
-	{
-		
-	}
-	
-	/**
 	 * Saves data to a specified file
 	 * @param filepath Filepath to save to
 	 */
@@ -77,12 +61,13 @@ public class ScholarshipModel {
 	}
 
 	public ArrayList<Scholar> getScholars() { return scholars;}
+	public ArrayList<ScholarOrganization> getSerials(){ return scholarOrganizations;}
+	public ArrayList<Paper> getPapers(){ return papers;}
 	
 	public void addScholar(Scholar input){
 		scholars.add(input);
 		processEvent(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, "Add Scholar"));
 	}
-	
 	/**
 	 * Removes a specific scholar
 	 * @param name The name of the scholar in the form first middle last
@@ -96,14 +81,95 @@ public class ScholarshipModel {
 				s = eachScholar;
 		
 		if(s != null){
+			//Go through each paper and remove the scholar
+			ArrayList<Paper> papersToRemove = new ArrayList<Paper>();
+			for(Paper eachPaper: papers){
+				eachPaper.removeAuthor(s);
+				//If that paper doesn't have an author anymore, remove it
+				if(eachPaper.getAuthors().isEmpty())
+					papersToRemove.add(eachPaper);
+			}
+			for(Paper eachPaperToRemove: papersToRemove)
+				papers.remove(eachPaperToRemove);
+			
+			//Go through each serial and remove the scholar
+			ArrayList<ScholarOrganization> serialsToRemove = new ArrayList<ScholarOrganization>();
+			for(ScholarOrganization eachSerial: scholarOrganizations){
+				if(eachSerial instanceof Conference){
+					for(Meeting eachMeeting: ((Conference) eachSerial).getMeetingCollection()){
+						eachMeeting.removeProgramChair(s);
+						eachMeeting.removeCommitteeMember(s);
+						
+						//If this serial doesn't have a program chair or committee member anymore, remove it
+						if(eachMeeting.getChairs().isEmpty() || eachMeeting.getCommittee().isEmpty())
+							serialsToRemove.add(eachSerial);
+					}
+				}
+				if(eachSerial instanceof Journal){
+					for(Volume eachVolume: ((Journal) eachSerial).getVolumeCollection()){
+						for(Issue eachIssue: eachVolume.getIssueCollection()){
+							eachIssue.removeEditor(s);
+							eachIssue.removeReviewer(s);
+							
+							//If this Serial doesn't have an editor or reviewer member anymore, remove it
+							if(eachIssue.getEditors().isEmpty() || eachIssue.getReviewers().isEmpty())
+								serialsToRemove.add(eachSerial);
+						}
+					}
+				}
+			}
+			for(ScholarOrganization eachSerialToRemove: serialsToRemove)
+				scholarOrganizations.remove(eachSerialToRemove);
+			
 			scholars.remove(s);
 			processEvent(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, "Remove Scholar"));
 		}
 	}
-	
 	public void removeAllScholars(){
 		scholars.clear();
 		processEvent(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, "Remove All Scholars"));
+	}
+	public void addSerial(ScholarOrganization input){
+		scholarOrganizations.add(input);
+		processEvent(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, "Add Serial"));
+	}
+	public void removeSerial(String name){
+		ScholarOrganization s = null;
+		
+		//Find the serial by name and then remove it
+		for(ScholarOrganization eachSerial: scholarOrganizations)
+			if(eachSerial.getName().equalsIgnoreCase(name))
+				s = eachSerial;
+		
+		if(s != null){
+			scholarOrganizations.remove(s);
+			processEvent(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, "Remove Serial"));
+		}
+	}
+	public void removeAllSerials(){
+		scholarOrganizations.clear();
+		processEvent(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, "Remove All Serials"));
+	}
+	public void addPaper(Paper input){
+		papers.add(input);
+		processEvent(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, "Add Paper"));
+	}
+	public void removePaper(String title){
+		Paper s = null;
+		
+		//Find the paper by title and then remove it
+		for(Paper eachPaper: papers)
+			if((eachPaper.getTitle().equalsIgnoreCase(title)))
+				s = eachPaper;
+		
+		if(s != null){
+			papers.remove(s);
+			processEvent(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, "Remove Paper"));
+		}
+	}
+	public void removeAllPapers(){
+		papers.clear();
+		processEvent(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, "Remove All Papers"));
 	}
 	
 	private void processEvent(ActionEvent e){
