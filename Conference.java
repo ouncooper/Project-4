1b95e4e28344e741c969984dbd9a1322a16c8394
Narkidae <connorbtaylor@gmail.com>
2013-11-13 19:49:55 -0600

    Update Conference.java

--- Conference.java
+++ Conference.java
@@ -1,43 +1,29 @@
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Comparator;
 
-public class ConferencePaper extends Paper implements Serializable
-{
-	private static final long serialVersionUID = 8507820325335680360L;
 
-	private Conference conferenceReference;
+public class Conference extends ScholarOrganization{
+	
+	private ArrayList<Meeting> meetingCollection;
 	
 	/**
-	 * Default constructor for the class.
+	 * Default constructor for the class
 	 */
-	public ConferencePaper() {}
+	public Conference() { }
 	
 	/**
-	 * Inherits from the Paper class.
-	 * Creates a ConferencePaper Object
-	 * @param newConferenceReference Reference to the conference the paper was published at
-	 * @param newAuthors Authors of the paper in the format Last, First Middle; Last, First Middle; etc.
-	 * @param newTitle Title of the paper.
-	 * @param newPageRange Page range specified in the file.
-	 * @param newDOI Digital Object Identifier for the paper.
+	 * Preferred constructor for the class
+	 * @param newMeetingCollection Meetings at the conference
+	 * @param organizationName The name of the organization
 	 */
-	public ConferencePaper(Conference newConferenceReference, ArrayList<Scholar> newAuthors, String newTitle, String newPageRange, String newDOI)
-	{
-		conferenceReference = newConferenceReference;
-		for(Scholar eachScholar: newAuthors)
-			addAuthor(eachScholar);
-		setTitle(newTitle);
-		setPageRange(newPageRange);
-		setDOI(newDOI);
+	public Conference(ArrayList<Meeting> newMeetingCollection, String organizationName){
+		meetingCollection = newMeetingCollection;
+		this.setName(organizationName);
 	}
 	
 	/**
-	 * @return The conference this paper was published at
+	 * @return Meetings at the conference
 	 */
-	public Conference getConference()
-	{
-		return conferenceReference;
+	public ArrayList<Meeting> getMeetingCollection(){
+		return meetingCollection;
 	}
-	
 }
